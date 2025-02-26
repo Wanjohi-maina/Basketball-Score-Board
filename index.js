715f0ee0da1fc9a0c26b7db7e1fd24492073ca9c
Wanjohi-maina <168757451+Wanjohi-maina@users.noreply.github.com>
2025-02-26 15:14:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 let homeScore = 0
 let guestScore = 0
-let homeScoreEl = document.getElementById("home-score")
-let guestScoreEl = document.getElementById("guest-score") 
-let homeScoreBtnOne = document.getElementById("home-score-btn-1")
-let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
-let homeScoreBtnThree = document.getElementById("home-score-btn-3")
-let guestScoreBtnOne = document.getElementById("guest-score-btn-1")
-let guestScoreBtnTwo = document.getElementById("guest-score-btn-2")
-let guestScoreBtnThree = document.getElementById("guest-score-btn-3")
-let resetEl = document.getElementById("reset-btn")
+const homeScoreEl = document.getElementById("home-score")
+const guestScoreEl = document.getElementById("guest-score") 
+const resetEl = document.getElementById("reset-btn")
 
 //  HOME FUNCTION
 function increaseHomeScoreOne() {
